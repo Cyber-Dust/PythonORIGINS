3ebbe9320e77abaf92a178c0c5fa2f5b1202d175
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-06 02:37:03 -0700

    Create BMI.py

---
+++ BMI.py
@@ -0,0 +1,4 @@
+# BMI CALCUALATOR
+height = float(input("Input your height in Feet: "))
+weight = float(input("Input your weight in Kilogram: "))
+print("Your body mass index is: ", round(weight / (height * height), 2))
